be88538e04ababc06347d54f4c4b6e92871ec8d0
paulamoyani <paulamoyani@gmail.com>
2018-03-13 13:45:28 -0400

    Updated database

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8888
--- Generation Time: Mar 13, 2018 at 06:34 PM
+-- Generation Time: Mar 13, 2018 at 06:44 PM
 -- Server version: 5.6.34
 -- PHP Version: 5.6.28
 
@@ -22,20 +22,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `queue` (
   `person` text,
-  `ind` int(11) NOT NULL
+  `ind` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `queue`
 --
 
-INSERT INTO `queue` (`person`, `ind`) VALUES
-('Jim', 1),
-('Ru', 2),
-('Luping', 3),
-('Liye', 4),
-('Elise', 5),
-('Paula', 6);
+INSERT INTO `queue` (`person`, `ind`, `time`) VALUES
+('Jim', 1, '2018-03-13 17:44:10'),
+('Ru', 2, '2018-03-13 17:44:10'),
+('Luping', 3, '2018-03-13 17:44:11'),
+('Liye', 4, '2018-03-13 17:44:11'),
+('Elise', 5, '2018-03-13 17:44:12'),
+('Paula', 6, '2018-03-13 17:44:12');
 
 --
 -- Indexes for dumped tables
